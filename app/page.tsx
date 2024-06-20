363a9e93f159a756faa9db33af389f03af715392
Hamed Bahram <hamed.bahram@gmail.com>
2024-06-20 12:33:16 -0400

    Add react-resizable

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Resizable from '@/components/resizable'
+
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Next shadcn starter</h1>
+        <h1 className='text-3xl font-bold'>React Resizable Panels</h1>
+        <p className='text-gray-500'>By Brian Vaughn</p>
+
+        <Resizable />
       </div>
     </section>
   )
---
+++ components/resizable.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { DragHandleDots2Icon } from '@radix-ui/react-icons'
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+
+export default function Resizable() {
+  return (
+    <PanelGroup
+      autoSaveId='example'
+      direction='horizontal'
+      className='mt-16 min-h-60 rounded-lg border'
+    >
+      <Panel defaultSize={25}>
+        <div className='flex h-full items-center justify-center bg-pink-100 p-6'>
+          <span className='font-semibold'>Sidebar</span>
+        </div>
+      </Panel>
+
+      <PanelResizeHandle className='flex w-px items-center justify-center border border-white'>
+        <div className='z-10 flex h-6 w-4 items-center justify-center rounded-sm border bg-zinc-200'>
+          <DragHandleDots2Icon className='h-4 w-4' />
+        </div>
+      </PanelResizeHandle>
+
+      <Panel defaultSize={25}>
+        <div className='flex h-full items-center justify-center bg-pink-100 p-6'>
+          <span className='font-semibold'>Content</span>
+        </div>
+      </Panel>
+    </PanelGroup>
+  )
+}
